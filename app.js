107cf23530b696c0ba3138248e5d09a0d57b81b2
Oscar Cauich <ocauich@gravitypayments.com>
2017-04-17 15:36:02 -0700

    created pictures array and created function to randomly select an array index and display it to the browser

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('It works');
+
+var pictureArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg'];
+
+var displayPictureArray = document.getElementById('pictureArrays');
+
+function randomArrayCreateIndex () {
+  var randomArrayIndex = pictureArray[Math.floor(Math.random()* pictureArray.length)];
+  return randomArrayIndex;
+}
+
+displayPictureArray.innerHTML = '<img src="img/' + randomArrayCreateIndex() + '" />';
+
+console.log(randomArrayCreateIndex());
