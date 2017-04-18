86739421e033967d8cf8265afe458e0b47390037
Oscar Cauich <ocauich@gravitypayments.com>
2017-04-17 23:15:13 -0700

    sucessfully able to display 3 random pictures on the browser

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
 'use strict';
 
-console.log('It works');
-
 var pictureArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg','breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif','water-can.jpg','wine-glass.jpg'];
 
-var displayPictureArrayOne = document.getElementById('pictures-Box-1');
-var displayPictureArrayTwo = document.getElementById('pictures-Box-2');
-var displayPictureArrayThree = document.getElementById('pictures-Box-3');
 
-function randomArrayCreateIndex () {
-  var randomArrayIndex = pictureArray[Math.floor(Math.random()* pictureArray.length)];
-  return randomArrayIndex;
+function Picture(name, path) {
+  this.name = name;
+  this.path = path;
+  this.numberOfTimesShowned = 0;
+  this.numberOfTimesClicked = 0;
+  this.generateRandonImage();
 }
 
-displayPictureArrayOne.innerHTML = '<img src="img/' + randomArrayCreateIndex() + '" />';
-displayPictureArrayTwo.innerHTML = '<img src="img/' + randomArrayCreateIndex() + '" />';
-displayPictureArrayThree.innerHTML = '<img src="img/' + randomArrayCreateIndex() + '" />';
+Picture.prototype.generateRandonImage = function () {
+  var generatedPicture = pictureArray[Math.floor(Math.random()* pictureArray.length)];
+  if(pictureArray.indexOf(generatedPicture == -1)) {
+    this.name = generatedPicture;
+    this.path = 'img/' + this.name;
+    // console.log(this.path)
+  } else {
+    this.generateRandonImage();
+  }
+  this.divIdCreate();
+  this.numberOfTimesShowned++;
+};
 
-console.log(randomArrayCreateIndex());
+Picture.prototype.divIdCreate = function () {
+  var displayPictureBoxes = document.getElementById('pictureArrays');
+  for(var i = 0; i < 1; i++){
+    var pictureDivCreate = document.createElement('div');
+    pictureDivCreate.setAttribute('id', this.name);
+    pictureDivCreate.className = 'pictures-Box';
+
+    var pictureImgTag = document.createElement('img');
+    pictureImgTag.src = this.path;
+    pictureDivCreate.appendChild(pictureImgTag);
+    displayPictureBoxes.appendChild(pictureDivCreate);
+  }
+};
+
+function countPictureClicks(){
+  this.numberOfTimesClicked++;
+  pictureOne.generateRandonImage();
+  pictureTwo.generateRandonImage();
+  pictureThree.generateRandonImage();
+  console.log(test.numberOfTimesClicked);
+
+}
+
+var pictureOne = new Picture();
+var pictureTwo = new Picture();
+var pictureThree = new Picture();
+
+
+var test = document.getElementById(pictureOne.name);
+console.log(test.numberOfTimesShowned);
+// add event listener to table
+test.addEventListener("click", countPictureClicks);
